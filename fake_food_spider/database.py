1a3fc9ba9a8b5f2ec9322123ecaf9600bbb66792
Jose Antunes <antunes.jose.mf@gmail.com>
2017-11-07 21:51:38 +0000

    Sergeys comments

--- fake_food_spider/database.py
+++ fake_food_spider/database.py
@@ -10,16 +10,14 @@ class StartUrl(Base):
     url = Column(String(1000))
     url_hash = Column(String(100))
     name = Column(String(100))
-    date = Column(DateTime)
+    date = Column(DateTime, default=datetime.utcnow())
     is_recipe = Column(Boolean)
 
-    def __init__(self, id=None, name=None, url=None,
-                 url_hash=None, date=datetime.utcnow(), is_recipe=False):
+    def __init__(self, id=None, name=None, url=None, url_hash=None, is_recipe=False):
         self.id = id
         self.url = url
         self.url_hash = url_hash
         self.name = name
-        self.date = date
         self.is_recipe = is_recipe
 
     def __repr__(self):
--- fake_food_spider/pipelines.py
+++ fake_food_spider/pipelines.py
@@ -4,6 +4,8 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+from sqlalchemy.exc import IntegrityError, DataError, DatabaseError
+
 from .connection import db
 from .database import StartUrl
 from .items import FakeFoodStartURL
