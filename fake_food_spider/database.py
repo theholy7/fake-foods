718dd02a8aa6cecb47eccff9c99c5fab648f31c6
Jose Antunes <antunes.jose.mf@gmail.com>
2017-11-07 22:01:10 +0000

    Recipe SQLAlchemy Model

--- fake_food_spider/database.py
+++ fake_food_spider/database.py
@@ -22,3 +22,33 @@ class StartUrl(Base):
 
     def __repr__(self):
         return "<URL: name={}, url={}>".format(self.name, self.url)
+
+
+class Recipe(Base):
+    __tablename__ = 'recipes'
+
+    id = Column(Integer, primary_key=True)
+    url = Column(String(1000))
+    url_hash = Column(String(100))
+    date_collected = Column(DateTime, default=datetime.utcnow())
+
+    name = Column(String(100))
+    date_published = Column(DateTime)
+
+    ingredients = Column(String(1000))
+    method = Column(String(2000))
+
+
+
+    def __init__(self, id=None, name=None, url=None, url_hash=None, date_published=None, ingredients=None, method=None):
+        self.id = id
+        self.url = url
+        self.url_hash = url_hash
+        self.name = name
+        self.date_published = date_published
+
+        self.ingredients = ingredients
+        self.method = method
+
+    def __repr__(self):
+        return "<RECIPE: name={}, url={}>".format(self.name, self.url)
