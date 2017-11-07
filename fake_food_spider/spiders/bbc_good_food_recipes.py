c9a7e1c136229f4450d275128807bc79a5f59286
Jose Antunes <antunes.jose.mf@gmail.com>
2017-11-07 22:11:21 +0000

    WIP: Recipe Spider

--- fake_food_spider/spiders/bbc_good_food_recipes.py
+++ fake_food_spider/spiders/bbc_good_food_recipes.py
@@ -1,11 +1,19 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from ..database import StartUrl
+
 
 
 class BbcGoodFoodRecipesSpider(scrapy.Spider):
     name = 'bbc_good_food_recipes'
     allowed_domains = ['bbcgoodfood.com']
     start_urls = ['http://bbcgoodfood.com/']
+
+    def start_requests(self):
+        bbc_start_urls = (db.query(StartUrl)
+                            .first())
+        
+
 
     def parse(self, response):
         pass
