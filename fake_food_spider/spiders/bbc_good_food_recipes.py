388a4d2ed8d096d394a1f70b122baa365486a5bc
Jose Antunes <antunes.jose.mf@gmail.com>
2017-12-03 13:26:09 +0000

    Overwrite start_requests to get urls from db

--- fake_food_spider/spiders/bbc_good_food_recipes.py
+++ fake_food_spider/spiders/bbc_good_food_recipes.py
@@ -1,18 +1,26 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from scrapy.http import Request
 from ..database import StartUrl
+from ..connection import db
 
 
 
 class BbcGoodFoodRecipesSpider(scrapy.Spider):
-    name = 'bbc_good_food_recipes'
+    name = 'bbc-good-food-recipes'
     allowed_domains = ['bbcgoodfood.com']
     start_urls = ['http://bbcgoodfood.com/']
 
     def start_requests(self):
+        # overwride start_requests to get urls from db
+
         bbc_start_urls = (db.query(StartUrl)
-                            .first())
-        
+                            .limit(5))
+
+        rqs = [ Request(url=start_url.url, callback=self.parse)
+                for start_url in bbc_start_urls]
+
+        return rqs
 
 
     def parse(self, response):
