ecf21e68e21ddeae3f1a2ac9627a3e99f0c56d80
Jose Antunes <antunes.jose.mf@gmail.com>
2017-12-20 14:04:57 +0000

    Less aggressive spider

--- fake_food_spider/settings.py
+++ fake_food_spider/settings.py
@@ -16,24 +16,24 @@ NEWSPIDER_MODULE = 'fake_food_spider.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'fake_food_spider (+http://www.yourdomain.com)'
+USER_AGENT = 'fake_food_spider'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 5
 
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
-#COOKIES_ENABLED = False
+# COOKIES_ENABLED = False
 
 # Disable Telnet Console (enabled by default)
 #TELNETCONSOLE_ENABLED = False
