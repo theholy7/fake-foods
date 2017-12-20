8637f38995f4174c7fc7abb6b08c41a688b6a5fc
Jose Antunes <antunes.jose.mf@gmail.com>
2017-12-20 16:38:53 +0000

    Add two basic proxies to the spiders

--- fake_food_spider/settings.py
+++ fake_food_spider/settings.py
@@ -52,9 +52,9 @@ DOWNLOAD_DELAY = 3
 
 # Enable or disable downloader middlewares
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
-# DOWNLOADER_MIDDLEWARES = {
-#    'fake_food_spider.middlewares.MyCustomDownloaderMiddleware': 543,
-#}
+DOWNLOADER_MIDDLEWARES = {
+   'fake_food_spider.middlewares.ProxyMiddleware': 543,
+}
 
 # Enable or disable extensions
 # See http://scrapy.readthedocs.org/en/latest/topics/extensions.html
