d81a1b4c79fcb58ad318c0bbd89ab7ae396cfe62
Jose Antunes <antunes.jose.mf@gmail.com>
2018-01-16 10:53:25 +0000

    Read vars from settings

--- fake_food_spider/connection.py
+++ fake_food_spider/connection.py
@@ -2,11 +2,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
+from scrapy.conf import settings
+
 # db settings
-dbuser = 'fake_foodie'
-dbpass = 'fake_food'
-dbhost = 'localhost'
-dbname = 'fake_foods'
+dbuser = settings['DB_USER']
+dbpass = settings['DB_PASS']
+dbhost = settings['DB_HOST']
+dbname = settings['DB_NAME']
 
 engine = create_engine("postgres://%s:%s@%s/%s"
                        % (dbuser, dbpass, dbhost, dbname),
