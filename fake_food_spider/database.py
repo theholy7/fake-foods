4635ffda449bd2aafb78f76bbc80c593cc03b0ec
Jose Antunes <antunes.jose.mf@gmail.com>
2018-01-18 15:29:27 +0000

    Add scrape count and scrape date to start url

--- fake_food_spider/database.py
+++ fake_food_spider/database.py
@@ -27,14 +27,18 @@ class StartUrl(Base):
     date = Column(DateTime, default=datetime.utcnow())
     is_recipe = Column(Boolean)
     s_id = Column(Integer)
+    scrape_count = Column(Integer, default=0)
+    scrape_date = Column(DateTime, default=datetime.utcnow())
 
-    def __init__(self, id=None, name=None, url=None, url_hash=None, is_recipe=False, s_id=0):
+    def __init__(self, id=None, name=None, url=None, url_hash=None,
+                 is_recipe=False, s_id=0, scrape_count=0):
         self.id = id
         self.url = url
         self.url_hash = url_hash
         self.name = name
         self.is_recipe = is_recipe
         self.s_id = s_id
+        self.scrape_count = scrape_count
 
     def __repr__(self):
         return "<URL: name={}, url={}>".format(self.name, self.url)
